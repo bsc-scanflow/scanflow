1356979ef53701ead94fddfdacb88843c566a3ef
peiniliu <peini.liu@gmail.com>
2021-06-25 16:00:51 +0200

    online inference graph

--- examples/mlperf/dataengineer/workflows/postprocessing-online/upload.py
+++ examples/mlperf/dataengineer/workflows/postprocessing-online/upload.py
@@ -3,6 +3,7 @@ import click
 import logging
 import pandas as pd
 import os
+import tensorflow as tf
 logger = logging.getLogger(__name__)
 
 import sys
@@ -11,8 +12,10 @@ from scanflow.client import ScanflowTrackerClient
 
 class upload(object):
 
-    def __init__(self, output=None, input=None, preprocess=None):
+    def __init__(self, 
+                 mlflow_log=None):
         print("Init upload")
+        self.mlflow_log = mlflow_log
         client = ScanflowTrackerClient()
 
         #log
@@ -20,18 +23,17 @@ class upload(object):
         logging.info("Connecting tracking server uri: {}".format(mlflow.get_tracking_uri()))
 
         mlflow.set_experiment("predictor")
-        with mlflow.start_run():
-            if output:
-                mlflow.log_artifacts(local_dir= output,
-                                     artifact_path= "output")
-            if input:
-                mlflow.log_artifacts(local_dir= input,
-                                     artifact_path= "input")
-            if preprocess:
-                mlflow.log_artifacts(local_dir= preprocess,
-                                     artifact_path= "preprocess")
+        with mlflow.start_run() as run:
+            self.run_id = run.info.run_id
+
+        self.epoch = 1
     
-    def predict(self, X):
+    def predict(self, X , names, meta):
+        with mlflow.start_run(run_id=self.run_id):
+            if self.mlflow_log:
+                mlflow.log_metric(key='num_of_predictions',
+                value=X.shape[0], step=self.epoch)
+                self.epoch = self.epoch + 1
         return X
         
     
