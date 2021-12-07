ef2b1c41753d2df400ab024dfa6c4ee2e1260c8c
peiniliu <peini.liu@gmail.com>
2021-12-07 23:11:41 +0100

    mnist exp remove verbose

--- examples/mnist/dataengineer/agents/planner/custom_actuators.py
+++ examples/mnist/dataengineer/agents/planner/custom_actuators.py
@@ -4,6 +4,12 @@ from typing import List
 from scanflow.client import ScanflowDeployerClient
 from scanflow.client import ScanflowTrackerClient
 
+import logging
+from scanflow.agent.config.httpClient import http_client
+from scanflow.server.schemas.app import Workflow
+import json
+
+
 import mlflow
 
 
@@ -36,3 +42,28 @@ async def call_update_workflow(run_id: str, artifact_path="data"):
                                         deployer="seldon")
     #update workflow
     await deployerClient.update_workflow("mnist", "dataengineer", workflow)
+    
+    
+#ok!
+async def call_deploy_workflow_replicas(app_name : str,
+                                        team_name : str,
+                                        workflow : Workflow,
+                                        kedaconfig : dict,
+                                        replicas : int,
+                                        deployer :str):
+    
+    #get workflow meta
+    workflow.kedaSpec = kedaconfig
+    
+    #deploy workflow
+    url = f"http://scanflow-server-service.scanflow-system.svc.cluster.local/deployer/deploy_workflow/{app_name}/{team_name}?replicas={replicas}&deployer={deployer}"
+    async with http_client.session.post(url, 
+                                        data=json.dumps(workflow.dict()),
+                                        headers={'Content-Type':'application/json'}) as response:
+                status = response.status
+                text = await response.json()
+    logging.info(f"{text['detail']}")
+    if status == 200:
+        return True
+    else:
+        return False
